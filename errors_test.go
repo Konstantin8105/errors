8ffb702d53cee11e999889eb78c50dcb600c204e
Konstantin <konstantin8105@gmail.com>
2019-05-16 10:56:41 +0300

    minimize external dependency (#2)
    * change tree library
    * remove unused snapshot
    * add function Walk
    * add tests for function Walk
    * go module

--- errors_test.go
+++ errors_test.go
@@ -3,12 +3,26 @@ package errors
 import (
 	"fmt"
 	"math"
+	"os"
 	"testing"
-
-	"github.com/bradleyjkemp/cupaloy"
 )
 
-func TestErrorTree(t *testing.T) {
+func TestNil(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Fatal(r)
+		}
+	}()
+	n := (*Tree)(nil)
+	n.Add(fmt.Errorf(""))
+
+	// walk
+	Walk(n, func(e error) {
+		fmt.Fprintf(os.Stdout, "%T %v\n", e, e)
+	})
+}
+
+func ExamplePrint() {
 	var et Tree
 	for i := 0; i < 10; i++ {
 		et.Add(fmt.Errorf("Error %d", i))
@@ -28,16 +42,95 @@ func TestErrorTree(t *testing.T) {
 			et.Add(ett)
 		}
 	}
-	t.Log(et.Error())
+	et.Add(fmt.Errorf("Multiline error:\nvalue is complex"))
 
-	if err := cupaloy.SnapshotMulti("Tree", et.Error()); err != nil {
-		t.Fatalf("error: %s", err)
-	}
+	// print error tree
+	fmt.Fprintf(os.Stdout, "%s\n", et.Error())
 
+	// walk
+	Walk(&et, func(e error) {
+		fmt.Fprintf(os.Stdout, "%T %v\n", e, e)
+	})
+
+	// reset
 	et.Reset()
 	if et.IsError() || len(et.errs) > 0 {
-		t.Fatalf("Reset is not working")
+		fmt.Fprintf(os.Stdout, "Reset is not working\n")
 	}
+
+	// Output:
+	// +
+	// ├──Error 0
+	// ├──+
+	// │  ├──Inside error 0
+	// │  └──Some deep deep errors
+	// │     └──Deep error 0
+	// ├──Error 1
+	// ├──Error 2
+	// ├──Error 3
+	// ├──+
+	// │  ├──Inside error 0
+	// │  ├──Some deep deep errors
+	// │  │  └──Deep error 0
+	// │  └──Inside error 1
+	// ├──Error 4
+	// ├──Error 5
+	// ├──Error 6
+	// ├──+
+	// │  ├──Inside error 0
+	// │  ├──Some deep deep errors
+	// │  │  └──Deep error 0
+	// │  ├──Inside error 1
+	// │  ├──Inside error 2
+	// │  └──Some deep deep errors
+	// │     ├──Deep error 0
+	// │     └──Deep error 1
+	// ├──Error 7
+	// ├──Error 8
+	// ├──Error 9
+	// ├──+
+	// │  ├──Inside error 0
+	// │  ├──Some deep deep errors
+	// │  │  └──Deep error 0
+	// │  ├──Inside error 1
+	// │  ├──Inside error 2
+	// │  ├──Some deep deep errors
+	// │  │  ├──Deep error 0
+	// │  │  └──Deep error 1
+	// │  └──Inside error 3
+	// └──Multiline error:
+	//    value is complex
+	//
+	// *errors.errorString Error 0
+	// *errors.errorString Inside error 0
+	// *errors.errorString Deep error 0
+	// *errors.errorString Error 1
+	// *errors.errorString Error 2
+	// *errors.errorString Error 3
+	// *errors.errorString Inside error 0
+	// *errors.errorString Deep error 0
+	// *errors.errorString Inside error 1
+	// *errors.errorString Error 4
+	// *errors.errorString Error 5
+	// *errors.errorString Error 6
+	// *errors.errorString Inside error 0
+	// *errors.errorString Deep error 0
+	// *errors.errorString Inside error 1
+	// *errors.errorString Inside error 2
+	// *errors.errorString Deep error 0
+	// *errors.errorString Deep error 1
+	// *errors.errorString Error 7
+	// *errors.errorString Error 8
+	// *errors.errorString Error 9
+	// *errors.errorString Inside error 0
+	// *errors.errorString Deep error 0
+	// *errors.errorString Inside error 1
+	// *errors.errorString Inside error 2
+	// *errors.errorString Deep error 0
+	// *errors.errorString Deep error 1
+	// *errors.errorString Inside error 3
+	// *errors.errorString Multiline error:
+	// value is complex
 }
 
 func ExampleTree() {
@@ -47,10 +140,27 @@ func ExampleTree() {
 	}
 	fmt.Println(et.Error())
 
+	// walk
+	Walk(et, func(e error) {
+		fmt.Fprintf(os.Stdout, "%T %v\n", e, e)
+	})
+
 	// Output:
 	// Check error tree
-	// ├── Error case 0
-	// └── Error case 1
+	// ├──Error case 0
+	// └──Error case 1
+	//
+	// *errors.errorString Error case 0
+	// *errors.errorString Error case 1
+}
+
+type ErrorValue struct {
+	ValueName string
+	Reason    error
+}
+
+func (e ErrorValue) Error() string {
+	return fmt.Sprintf("Value `%s`: %v", e.ValueName, e.Reason)
 }
 
 func Example() {
@@ -63,7 +173,10 @@ func Example() {
 	var et Tree
 	et.Name = "Check input data"
 	if math.IsNaN(f) {
-		et.Add(fmt.Errorf("Parameter `f` is NaN"))
+		et.Add(ErrorValue{
+			ValueName: "f",
+			Reason:    fmt.Errorf("is NaN"),
+		})
 	}
 	if f < 0 {
 		et.Add(fmt.Errorf("Parameter `f` is negative"))
@@ -79,9 +192,18 @@ func Example() {
 		fmt.Println(et.Error())
 	}
 
+	// walk
+	Walk(&et, func(e error) {
+		fmt.Fprintf(os.Stdout, "%-25s %v\n", fmt.Sprintf("%T", e), e)
+	})
+
 	// Output:
 	// Check input data
-	// ├── Parameter `f` is NaN
-	// ├── Parameter `i` is less zero
-	// └── Parameter `s` is empty
+	// ├──Value `f`: is NaN
+	// ├──Parameter `i` is less zero
+	// └──Parameter `s` is empty
+	//
+	// errors.ErrorValue         Value `f`: is NaN
+	// *errors.errorString       Parameter `i` is less zero
+	// *errors.errorString       Parameter `s` is empty
 }
